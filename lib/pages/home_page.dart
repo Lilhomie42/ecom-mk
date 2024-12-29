c5ee14ea2caf29d939f3a331d6b595d57378b60a
CristianM <mcris740@gmail.com>
2024-12-29 14:30:08 -0500

    Added a drawer widget with icons buttons

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,7 +11,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   int _selectedIndex = 0;
 
   void navigateBottomBar(int index) {
@@ -22,7 +21,6 @@ class _HomePageState extends State<HomePage> {
 
   final List<Widget> _pages = [
     const ShopPage(),
-
     const CartPage(),
   ];
 
@@ -30,8 +28,95 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      bottomNavigationBar: MyBottomNavBar(
-        onTabChange: (index) => navigateBottomBar(index)
+      bottomNavigationBar:
+          MyBottomNavBar(onTabChange: (index) => navigateBottomBar(index)),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(
+            builder: (context) => IconButton(
+                  icon: const Icon(
+                    Icons.menu_rounded,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                )),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey.shade800,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // Logo
+            Column(
+              children: [
+                DrawerHeader(
+                  child: Image.asset('lib/images/Ecomlogo.png'),
+                ),
+                
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Divider(
+                    color: Colors.grey.shade800,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.home_outlined,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'HOME',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Divider(
+                    color: Colors.grey.shade800,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.info_outline,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'ABOUT',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Divider(
+                    color: Colors.grey.shade800,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 25.0, bottom: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.logout_rounded,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  'LOGOUT',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
       body: _pages[_selectedIndex],
     );
