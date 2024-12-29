d08743e73f2c5cc966294ffd7ee311e5db17353f
CristianM <mcris740@gmail.com>
2024-12-29 10:52:00 -0500

    Added text and UI for a button

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -7,28 +7,70 @@ class IntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: Column(
-        children: [
-          // Logo
-          Padding(
-            padding: const EdgeInsets.only(top: 60, left: 25, right: 25),
-            child: Center(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.asset(
-                                'lib/images/Ecomlogo.png',
-                                width: MediaQuery.of(context).size.width * 8.0,
-                                fit: BoxFit.contain,
-                              ),
-                )),
-          ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          children: [
+            // Logo
+            Padding(
+              padding: const EdgeInsets.only(top: 65, left: 25, right: 25),
+              child: Center(
+                  child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(
+                  'lib/images/Ecomlogo.png',
+                  width: MediaQuery.of(context).size.width * 8.0,
+                  fit: BoxFit.contain,
+                ),
+              )),
+            ),
 
-          // Title
+            const SizedBox(height: 48),
 
-          // Sub-title
+            // Title
 
-          //Start now button
-        ],
+            Text(
+              'Just Do It',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+
+            const SizedBox(height: 24.0),
+            // Sub-title
+
+            Text(
+              'Brand new sneakers made with premium quality materials.',
+              style: TextStyle(
+                color: Colors.grey.shade700,
+                fontSize: 16,
+              ),
+              textAlign: TextAlign.center,
+            ),
+
+            const SizedBox(height: 48),
+            //Start now button
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.grey[900],
+                  borderRadius: BorderRadius.circular(12)),
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Center(
+                  child: Text(
+                    "Shop Now",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
