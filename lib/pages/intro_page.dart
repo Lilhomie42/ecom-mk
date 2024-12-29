374ab033f7073964c7f6eede7048b4a9a325f916
CristianM <mcris740@gmail.com>
2024-12-29 10:17:37 -0500

    Added logo example with sizing

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -7,7 +7,29 @@ class IntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
+      body: Column(
+        children: [
+          // Logo
+          Padding(
+            padding: const EdgeInsets.only(top: 60, left: 25, right: 25),
+            child: Center(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                                'lib/images/Ecomlogo.png',
+                                width: MediaQuery.of(context).size.width * 8.0,
+                                fit: BoxFit.contain,
+                              ),
+                )),
+          ),
+
+          // Title
+
+          // Sub-title
+
+          //Start now button
+        ],
+      ),
     );
   }
 }
-
